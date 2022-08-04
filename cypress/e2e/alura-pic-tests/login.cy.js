9d6ddf4772529e32ae59dabcaadc9c619c218527
Lucas Smaniotto Schuch <lucas.schuch@expressosaomiguel.com.br>
2022-08-04 11:19:17 -0300

    Boas práticas & Mocks e Stubs

--- cypress/e2e/alura-pic-tests/login.cy.js
+++ cypress/e2e/alura-pic-tests/login.cy.js
@@ -2,10 +2,15 @@ describe('Testes de Login na ALURAPIC', () => {
 
     beforeEach(() => {
         cy.visit('https://alura-fotos.herokuapp.com/#/home');
+
+        cy.intercept('POST', 'https://apialurapic.herokuapp.com/user/login', {
+            statusCode: 400
+        }).as('stubPost')
     })
 
     it('Faz login de usuário válido', () => {
-        cy.login('flavio', '123');
+        cy.login(Cypress.env('userName'), Cypress.env('password'));
+        cy.wait('@stubPost')
         cy.contains('a', '(Logout)').should('be.visible');
     })
 
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('login', (name, password) =>{
     cy.get('input[formcontrolname="userName"]').type(name);
-    cy.get('input[formcontrolname="password"]').type(password);
+    cy.get('input[formcontrolname="password"]').type(password, {log: false});
     cy.get('button[type="submit"]').click();
 })
 
